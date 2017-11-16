542ec17c4a18766db9af27e1ab5c541a35d2365c
Steffen Matthischke <steffen.matthischke@lovoo.com>
2017-11-16 21:30:20 +0100

    Initial Commit

---
+++ AppCenterEva/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppCenterEva
+//
+//  Created by Steffen Matthischke on 16.11.17.
+//  Copyright © 2017 Steffen Matthischke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
